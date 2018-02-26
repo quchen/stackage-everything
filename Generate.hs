7bc705d1163ecb64ab9632b2c5796c497b380347
David Luposchainsky <dluposchainsky@gmail.com>
2018-02-26 15:27:26 +0100

    Housekeeping; make version number parser more robust

--- Generate.hs
+++ Generate.hs
@@ -19,6 +19,7 @@ module Main (main) where
 
 
 
+import           Control.Applicative
 import           Control.Lens
 import qualified Data.ByteString.Lazy                  as BSL
 import           Data.Semigroup
@@ -28,16 +29,21 @@ import qualified Data.Text.Encoding                    as T
 import qualified Data.Text.IO                          as T
 import           Data.Text.Prettyprint.Doc
 import           Data.Text.Prettyprint.Doc.Render.Text
+import           Data.Void
 import           Network.Wreq
 import           System.Environment
 import           System.Exit
 import           System.IO
 import           Text.Megaparsec                       as P
 import           Text.Megaparsec.Char                  as P
+import           Text.Read                             (readMaybe)
 
 
 
-newtype LtsVersion = LtsVersion Text deriving (Eq, Ord, Show)
+data LtsVersion = LtsVersion Int Int deriving (Eq, Ord, Show)
+ltsTextVersion :: LtsVersion -> Text
+ltsTextVersion (LtsVersion major minor) = T.pack (show major) <> "." <> T.pack (show minor)
+
 newtype Name = Name Text deriving (Eq, Ord, Show)
 data Version = Version Text | NoVersion deriving (Eq, Ord, Show)
 
@@ -46,52 +52,68 @@ main = do
     args <- getArgs
     case args of
         ltsFlag : ltsVersion : [] | ltsFlag == "--lts"
-            -> generateScript (LtsVersion (T.pack ltsVersion))
+            -> case parseLtsVersion (T.pack ltsVersion) of
+                Left err -> do
+                    T.hPutStrLn stderr "Invalid version specified"
+                    T.hPutStrLn stderr err
+                    exitWith (ExitFailure 1)
+                Right v -> generateScript v
         _other -> do
             T.hPutStrLn stderr "Usage: ./Generate.hs --lts <version>"
             T.hPutStrLn stderr "Version: e.g. 10.5 for LTS-10.5"
             exitWith (ExitFailure 1)
 
+parseLtsVersion :: Text -> Either Text LtsVersion
+parseLtsVersion input = case parse version "<LTS version parameter>" input of
+    Left err -> Left (T.pack (parseErrorPretty err))
+    Right r  -> Right r
+  where
+    version :: Parsec Void Text LtsVersion
+    version = do
+        (major', minor') <- (,) <$> some digitChar <* char '.' <*> some digitChar <* eof
+        case liftA2 (,) (readMaybe major') (readMaybe minor') of
+            Nothing -> fail (T.unpack ("Error: " <> input <> " is not a valid LTS version number. Format: 12.3"))
+            Just (major, minor) -> pure (LtsVersion major minor)
+
 downloadCabalConstraints :: LtsVersion -> IO Text
-downloadCabalConstraints (LtsVersion lts) = do
-    response <- get ("https://www.stackage.org/lts-" ++ T.unpack lts ++ "/cabal.config")
+downloadCabalConstraints ltsVersion = do
+    response <- get ("https://www.stackage.org/lts-" ++ T.unpack (ltsTextVersion ltsVersion) ++ "/cabal.config")
     pure (T.decodeUtf8 (BSL.toStrict (view responseBody response)))
 
-generateScript
-    :: LtsVersion
-    -> IO ()
-generateScript lts@(LtsVersion ltsVersion) = do
-    T.hPutStrLn stderr ("Downloading constraints for LTS-" <> ltsVersion)
-    contents <- downloadCabalConstraints lts
+generateScript :: LtsVersion -> IO ()
+generateScript ltsVersion = do
+    T.hPutStrLn stderr ("Downloading constraints for LTS-" <> ltsTextVersion ltsVersion)
+    contents <- downloadCabalConstraints ltsVersion
     T.hPutStrLn stderr "Parsing constraints file"
     case parse cabalConstraintFileP "" contents of
-        Left err -> T.putStrLn "Parse error: " >> print err
-        Right (Packages packages) ->
-            let render = renderIO stdout
-                layout = layoutPretty defaultLayoutOptions
-            in (render . layout . vcat)
-                [ "#!/usr/bin/env bash"
-                , ""
-                , "set -euo pipefail"
-                , ""
-                , hang 4 (backslashedLineBreaks
-                    (pretty ("stack build --dry-run --prefetch --resolver lts-" <> ltsVersion)
-                    : [pretty pkgName | (Name pkgName, _) <- packages
-                                      , pkgName /= "ghc" -- Tends to misbehave so we exclude it here
-                                      , pkgName /= "rts" -- Dito
-                                      ]))
-                , ""
-                ]
+        Left err       -> T.putStrLn "Parse error: " >> print err
+        Right packages -> renderScript ltsVersion packages
 
-backslashedLineBreaks :: [Doc ann] -> Doc ann
-backslashedLineBreaks = concatWith (\x y -> x <> " \\" <> line <> y)
+renderScript :: LtsVersion -> Packages -> IO ()
+renderScript ltsVersion (Packages packages) = (render . layout . vcat)
+    [ "#!/usr/bin/env bash"
+    , ""
+    , "set -euo pipefail"
+    , ""
+    , hang 4 (backslashedLineBreaks
+        (pretty ("stack build --dry-run --prefetch --resolver lts-" <> ltsTextVersion ltsVersion)
+        : [pretty pkgName | (Name pkgName, _) <- packages
+                          , pkgName /= "ghc" -- Tends to misbehave so we exclude it here
+                          , pkgName /= "rts" -- Dito
+                          ]))
+    , ""
+    ]
+  where
+    render = renderIO stdout
+    layout = layoutPretty defaultLayoutOptions
+    backslashedLineBreaks = concatWith (\x y -> x <> " \\" <> line <> y)
 
 -- | A list of packages and their respective versions.
 newtype Packages = Packages [(Name, Version)]
     deriving (Eq, Ord, Show)
 
 -- | Ad-hoc parser for handling Stackage provided Cabal constraint files.
-cabalConstraintFileP :: Parsec () Text Packages
+cabalConstraintFileP :: Parsec Void Text Packages
 cabalConstraintFileP = manyTill anyChar (try (string "constraints:"))
      *> P.space
      *> packagesP
@@ -99,12 +121,12 @@ cabalConstraintFileP = manyTill anyChar (try (string "constraints:"))
 
   where
 
-    packagesP :: Parsec () Text Packages
+    packagesP :: Parsec Void Text Packages
     packagesP = fmap Packages (sepBy packageP comma')
       where
         comma' = char ',' *> P.space
 
-    packageP :: Parsec () Text (Name, Version)
+    packageP :: Parsec Void Text (Name, Version)
     packageP = do
         name <- do n <- some (alphaNumChar <|> char '-')
                    (pure . Name . T.pack) n
