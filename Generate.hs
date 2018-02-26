94ff87d51ebee25d016afd22fad6a158a0217cd5
David Luposchainsky <dluposchainsky@gmail.com>
2018-02-26 14:51:02 +0100

    Generate a single stack call only, dramatically speeding up script execution

--- Generate.hs
+++ Generate.hs
@@ -1,9 +1,10 @@
 #!/usr/bin/env stack
-{- stack --resolver lts-9.0 --install-ghc runghc
+{- stack --resolver lts-10.7 --install-ghc runghc
     --package base
     --package bytestring
     --package lens
     --package megaparsec
+    --package prettyprinter
     --package text
     --package wreq
     --
@@ -19,22 +20,24 @@ module Main (main) where
 
 
 import           Control.Lens
-import qualified Data.ByteString.Lazy as BSL
-import           Data.Foldable
-import           Data.Monoid
-import           Data.Text            (Text)
-import qualified Data.Text            as T
-import qualified Data.Text.Encoding   as T
-import qualified Data.Text.IO         as T
+import qualified Data.ByteString.Lazy                  as BSL
+import           Data.Semigroup
+import           Data.Text                             (Text)
+import qualified Data.Text                             as T
+import qualified Data.Text.Encoding                    as T
+import qualified Data.Text.IO                          as T
+import           Data.Text.Prettyprint.Doc
+import           Data.Text.Prettyprint.Doc.Render.Text
 import           Network.Wreq
 import           System.Environment
 import           System.Exit
 import           System.IO
-import           Text.Megaparsec
-import           Text.Megaparsec.Text
+import           Text.Megaparsec                       as P
+import           Text.Megaparsec.Char                  as P
 
 
 
+newtype LtsVersion = LtsVersion Text deriving (Eq, Ord, Show)
 newtype Name = Name Text deriving (Eq, Ord, Show)
 data Version = Version Text | NoVersion deriving (Eq, Ord, Show)
 
@@ -43,57 +46,69 @@ main = do
     args <- getArgs
     case args of
         ltsFlag : ltsVersion : [] | ltsFlag == "--lts"
-            -> generateScript (T.pack ltsVersion)
+            -> generateScript (LtsVersion (T.pack ltsVersion))
         _other -> do
             T.hPutStrLn stderr "Usage: ./Generate.hs --lts <version>"
             T.hPutStrLn stderr "Version: e.g. 10.5 for LTS-10.5"
             exitWith (ExitFailure 1)
 
-downloadCabalConstraints :: Text -> IO Text
-downloadCabalConstraints lts = do
+downloadCabalConstraints :: LtsVersion -> IO Text
+downloadCabalConstraints (LtsVersion lts) = do
     response <- get ("https://www.stackage.org/lts-" ++ T.unpack lts ++ "/cabal.config")
     pure (T.decodeUtf8 (BSL.toStrict (view responseBody response)))
 
 generateScript
-    :: Text     -- ^ Stackage LTS version to target.
+    :: LtsVersion
     -> IO ()
-generateScript ltsVersion = do
+generateScript lts@(LtsVersion ltsVersion) = do
     T.hPutStrLn stderr ("Downloading constraints for LTS-" <> ltsVersion)
-    contents <- downloadCabalConstraints ltsVersion
+    contents <- downloadCabalConstraints lts
     T.hPutStrLn stderr "Parsing constraints file"
     case parse cabalConstraintFileP "" contents of
         Left err -> T.putStrLn "Parse error: " >> print err
-        Right (Packages packages) -> do
-            T.putStrLn "#!/usr/bin/env bash"
-            T.putStrLn ""
-            T.putStrLn "set -veuo pipefail"
-            T.putStrLn ""
-            for_ packages (\(Name pkgName, _pkgVersion) ->
-                T.putStrLn ("stack build --dry-run --prefetch --resolver lts-" <> ltsVersion <> " " <> pkgName) )
+        Right (Packages packages) ->
+            let render = renderIO stdout
+                layout = layoutPretty defaultLayoutOptions
+            in (render . layout . vcat)
+                [ "#!/usr/bin/env bash"
+                , ""
+                , "set -euo pipefail"
+                , ""
+                , hang 4 (backslashedLineBreaks
+                    (pretty ("stack build --dry-run --prefetch --resolver lts-" <> ltsVersion)
+                    : [pretty pkgName | (Name pkgName, _) <- packages
+                                      , pkgName /= "ghc" -- Tends to misbehave so we exclude it here
+                                      , pkgName /= "rts" -- Dito
+                                      ]))
+                , ""
+                ]
+
+backslashedLineBreaks :: [Doc ann] -> Doc ann
+backslashedLineBreaks = concatWith (\x y -> x <> " \\" <> line <> y)
 
 -- | A list of packages and their respective versions.
 newtype Packages = Packages [(Name, Version)]
     deriving (Eq, Ord, Show)
 
 -- | Ad-hoc parser for handling Stackage provided Cabal constraint files.
-cabalConstraintFileP :: Parser Packages
+cabalConstraintFileP :: Parsec () Text Packages
 cabalConstraintFileP = manyTill anyChar (try (string "constraints:"))
-     *> space
+     *> P.space
      *> packagesP
      <* manyTill anyChar eof
 
   where
 
-    packagesP :: Parser Packages
-    packagesP = fmap Packages (sepBy packageP comma)
+    packagesP :: Parsec () Text Packages
+    packagesP = fmap Packages (sepBy packageP comma')
       where
-        comma = char ',' *> space
+        comma' = char ',' *> P.space
 
-    packageP :: Parser (Name, Version)
+    packageP :: Parsec () Text (Name, Version)
     packageP = do
         name <- do n <- some (alphaNumChar <|> char '-')
                    (pure . Name . T.pack) n
-        space
+        P.space
         version <- do
             let versionNumber = do
                     _ <- string "=="
@@ -101,5 +116,5 @@ cabalConstraintFileP = manyTill anyChar (try (string "constraints:"))
                     pure (Version (T.pack v) )
                 merelyInstalled = NoVersion <$ string "installed"
             versionNumber <|> merelyInstalled
-        space
+        P.space
         pure (name, version)
